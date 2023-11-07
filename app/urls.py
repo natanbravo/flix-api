b2268bdc339b2165fb6bff88fc7920349b0bd74f
natanbravo <nobprogrammer8@gmail.com>
2023-11-07 13:52:13 -0300

    feat: add genres crud

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import JsonResponse
 
-def hello_view(request):
-    return JsonResponse({'message':'E ai cuzão'})
+from genres.views import genre_create_list_view, genre_detail_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', hello_view)
+    path('genres/', genre_create_list_view, name='genre-list'),
+    path('genre/<int:pk>', genre_detail_view, name='genre-detail-view')
 ]
