37691c37598866622c03f34b243abb5a229c169d
natanbravo <nobprogrammer8@gmail.com>
2023-11-08 11:30:12 -0300

    feat: add movies crud

--- app/urls.py
+++ app/urls.py
@@ -2,10 +2,18 @@ from django.contrib import admin
 from django.urls import path
 
 from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
-
+from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroy
+from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('genres/', GenreCreateListView.as_view(), name='genre-list'),
-    path('genre/<int:pk>', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view')
+    path('genre/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
+
+    path('actors/', ActorCreateListView.as_view(), name='actor-create-list-view'),
+    path('actor/<int:pk>/', ActorRetrieveUpdateDestroy.as_view(), name='actor-detail-view'),
+
+    path('movies/', MovieCreateListView.as_view(), name='movie-create-list-view'),
+    path('movie/<int:pk>/', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-view')
 ]
