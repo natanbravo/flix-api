5ef7cd89ad0b18aa1d32e3841ae68d14e1c8c7fa
natanbravo <nobprogrammer8@gmail.com>
2023-11-08 16:09:07 -0300

    feat: add reviews crud

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
 from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroy
 from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
+from reviews.views import ReviewCreateListView, ReviewRetrieveUpdateDestroy
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,5 +17,8 @@ urlpatterns = [
     path('actor/<int:pk>/', ActorRetrieveUpdateDestroy.as_view(), name='actor-detail-view'),
 
     path('movies/', MovieCreateListView.as_view(), name='movie-create-list-view'),
-    path('movie/<int:pk>/', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-view')
+    path('movie/<int:pk>/', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-view'),
+
+    path('reviews/', ReviewCreateListView.as_view(), name='review-create-list-view'),
+    path('review/<int:pk>/', ReviewRetrieveUpdateDestroy.as_view(), name='review-detail-view')
 ]
