ca941593796d8d4438779cb4653396533b266f7e
natanbravo <nobprogrammer8@gmail.com>
2023-11-21 09:35:54 -0300

    feat: add calculated fields

--- movies/serializer.py
+++ movies/serializer.py
@@ -10,16 +10,28 @@ class MovieModelSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def get_rate(self, obj):
-        return 5
+        reviews = obj.reviews.all()
+
+        if reviews:
+            sum_reviews = 0
+
+            for review in reviews:
+                sum_reviews += review.stars
+
+            reviews_count = reviews.count()
+
+            return round(sum_reviews / reviews_count, 1)
+
+        return None
 
     def validate_release_date(self, value):
-        if value.year < 1990:
+        if value.year < 1900:
             raise serializers.ValidationError(
                 'A data de laçamento não pode ser anterior a 1990.')
         return value
 
     def validate_resume(self, value):
-        if len(value) > 200:
+        if len(value) > 500:
             raise serializers.ValidationError(
                 'Resumo não pode ter mais que 200 caractéres.')
         return value
